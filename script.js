6945ec48ad8e0a5007b42b9091ee45e9c3138204
Dean99712 <Dean2910997@gmail.com>
2022-09-08 17:51:31 +0300

    First init of the website

---
+++ script.js
@@ -0,0 +1,6 @@
+let id = document.getElementById("form-group--user-id");
+let idInput = document.getElementById("form-group--user-id-input")
+
+idInput.addEventListener("click", function(){
+    console.log("log id");
+})
